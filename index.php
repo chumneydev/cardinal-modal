2086c60ac38094ada46947410bbde5176de1de9f
Louis Stephens <lstephens@chumneyads.com>
2020-01-17 09:25:52 -0500

    Ajax object added
    Ajax object created to load in dynamic content to modal

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Cardinal Modal</title>
-    <link rel="stylesheet" href="dist/css/cardinal.css">
     <link rel="stylesheet" href="dist/css/home.css">
     <link rel="stylesheet" href="dist/css/modal.min.css">
 
@@ -14,13 +13,23 @@
 
 <body>
 
-    <div id="sidebar">
-        <ul>
-            <li><a href="#starting">Getting Started</a></li>
-            <li><a href="#grid">Grid</a></li>
-            <li><a href="#colors">Colors</a></li>
-            <li><a href="#updating">Updating</a></li>
-        </ul>
+    <div id="container">
+
+        <p>Donec ullamcorper nulla non metus auctor fringilla. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Curabitur blandit tempus porttitor. Nullam quis risus eget urna mollis ornare vel eu leo.</p>
+
+        <p>Sed posuere consectetur est at lobortis. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+
+        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Etiam porta sem malesuada magna mollis euismod. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+
+        <p>Nullam quis risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas faucibus mollis interdum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+
+
+    </div>
+
+
+
+    <div id="content">
+        <p>Etiam porta sem malesuada magna mollis euismod. Sed posuere consectetur est at lobortis. Donec sed odio dui. Donec id elit non mi porta gravida at eget metus. Donec sed odio dui. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
     </div>
 
 
@@ -31,20 +40,39 @@
     <!-- containter -->
 
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-    <script src="dist/js/cardinal.modal-min.js"></script>
+    <script src="src/js/cardinal.modal.js"></script>
 
 
 
 </body>
 <script type="text/javascript">
-    var myModal = new Modal({
+    //var content = "Nulla vitae elit libero, a pharetra augue. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nulla vitae elit libero, a pharetra augue.";
+
+    var content = document.getElementById('content');
+
+    /*var content = $.get('http://192.168.12.3:8888/development/projects/password/#', function(data) {
+        $('.ca-modal-content').html(data)
+    });*/
+
+    /*var caModal = new Modal({
+        maxWidth: 580,
         autoOpen: false,
-        content: 'howdy',
-        maxWidth: 600,
+        content: content
+    }); 
+    caModal.open();*/
+
+
+    var myModal = new Cardinal({
+        content: content,
+        overlay: true,
+        autoOpen: true,
+        //height: 200,
+        exitIntent: false,
+        ajaxURL: 'http://192.168.12.3:8888/internal-projectsdemos/content.php',
+        //cookieAmount: 2,
+        //cookieName: 'abcd',
+        //cookieValue: 'efgh',
     });
-
-    myModal.open();
-
 </script>
 
 </html>
--- src/js/cardinal.modal.js
+++ src/js/cardinal.modal.js
@@ -2,7 +2,7 @@
 (function () {
 
     // Define our constructor 
-    this.Modal = function () {
+    this.Cardinal = function () {
 
         // Create global element references
         this.closeButton = null;
@@ -12,6 +12,10 @@
         // Determine proper prefix
         this.transitionEnd = transitionSelect();
 
+        var _ = this;
+
+
+
         // Define option defaults 
         var defaults = {
             autoOpen: false,
@@ -20,7 +24,14 @@
             content: "",
             maxWidth: 600,
             minWidth: 280,
-            overlay: true
+            height: 0,
+            overlay: true,
+            ajaxURL: "",
+            cookieAmount: 0,
+            cookieName: '',
+            cookieValue: '',
+            exitIntent: false,
+            onOpening: function () {}, // function for ajax url
         }
 
         // Create options by extending defaults with the passed in arugments
@@ -28,13 +39,59 @@
             this.options = extendDefaults(defaults, arguments[0]);
         }
 
-        if (this.options.autoOpen === true) this.open();
+        if (this.options.autoOpen === true) {
+            this.open()
+            console.info('Chumney & Associates: Modal has opened')
+
+        }
+
+        // If exit intent is true, delay modal
+        if (this.options.exitIntent === true) {
+
+            var invokeModal = true;
+            document.addEventListener("mouseout", evt => {
+                if (evt.toElement === null && evt.relatedTarget === null) {
+                    if (invokeModal) {
+                        // An intent to exit has happened
+                        console.log("Chumney & Associates")
+                        this.open();
+                        invokeModal = false;
+                    }
+                }
+            });
+            console.log('Chumney & Associates: exit intent engaged');
+        }
+
+
+        /*if (this.options.cookie.cookieActive === true) {
+            console.log(this.options.cookie.cookieAmount)
+            createCookie(this.options.cookie.cookieName, this.options.cookie.cookieValue, this.options.cookie.cookieAmount);
+            //console.log(getCookie(this.options.cookie.cookieName))
+            console.log(accessCookie(this.options.cookie.cookieName))
+
+            // If cookie is set, then dont open
+            if (checkCookie(this.options.cookie.cookieName)) {
+
+
+            }
+
+            else {
+                removeCookie(this.options.cookie.cookieName)
+            }
+        }*/
+
+
+        // Ajax driven content
+        if (this.options.onOpening && typeof (this.options) === 'function') {
+            this.options.onOpening(this);
+        }
+
 
     }
 
     // Public Methods
 
-    Modal.prototype.close = function () {
+    Cardinal.prototype.close = function () {
         var _ = this;
         this.modal.className = this.modal.className.replace(" ca-modal-open", "");
         this.overlay.className = this.overlay.className.replace(" ca-modal-open",
@@ -44,18 +101,75 @@
         });
         this.overlay.addEventListener(this.transitionEnd, function () {
             if (_.overlay.parentNode) _.overlay.parentNode.removeChild(_.overlay);
+
         });
+        console.log('Chumney Modal has closed')
+
     }
 
-    Modal.prototype.open = function () {
+    Cardinal.prototype.open = function () {
         buildOut.call(this);
         initializeEvents.call(this);
         window.getComputedStyle(this.modal).height;
         this.modal.className = this.modal.className +
             (this.modal.offsetHeight > window.innerHeight ?
-                " ca-modal-open ca-modal-top": " ca-modal-open");
+                " ca-modal-open ca-modal-anchor" : " ca-modal-open");
         this.overlay.className = this.overlay.className + " ca-modal-open";
+
+        this.setCookie()
+        this.allCookies()
+        this.checkCookie()
+        this.ajaxLoadContent()
     }
+
+    // Load Content via ajax
+    Cardinal.prototype.ajaxLoadContent = function () {
+        var _ = this;
+
+        if (_.options.ajaxURL != '') {
+            _.options.content = $.get(_.options.ajaxURL, function(data) {
+                $('.ca-modal-content').html(data)
+                console.log('The content has been loaded into the html')
+            });
+        }
+
+    }
+
+
+    Cardinal.prototype.allCookies = function() {
+        var _ = this;
+
+        if (_.options.cookieAmount != 0) {
+            _.close();
+            console.log("liusehisuh")
+            console.log(_.checkCookie(_.options.cookieName))
+
+        }
+
+    }
+
+    Cardinal.prototype.setCookie = function (cookieAmount) {
+        var _ = this;
+
+        if (_.options.cookieAmount) {
+            createCookie(_.options.cookieName, _.options.cookieValue, _.options.cookieAmount, )
+            console.log("Hello!")
+        }
+    }
+
+
+    Cardinal.prototype.checkCookie = function (cookieName) {
+        var _ = this;
+
+        if (document.cookie.indexOf(_.options.cookieName) > -1) {
+            console.log('Cookie Name is: ' + _.options.cookieName);
+        }
+
+
+    }
+
+
+
 
     // Private Methods
 
@@ -83,6 +197,11 @@
         this.modal.style.minWidth = this.options.minWidth + "px";
         this.modal.style.maxWidth = this.options.maxWidth + "px";
 
+
+        // height
+        // this.modal.style.height = this.options.height + "px";
+
+
         // If closeButton option is true, add a close button
         if (this.options.closeButton === true) {
             this.closeButton = document.createElement("button");
@@ -97,6 +216,7 @@
             this.overlay.className = "ca-modal-overlay " + this.options.className;
             docFrag.appendChild(this.overlay);
         }
+
 
         // Create content area and append to modal
         contentHolder = document.createElement("div");
@@ -120,7 +240,58 @@
             }
         }
         return source;
+
     }
+
+
+
+    // Create a Cookie
+    function createCookie(cookieName, cookieValue, daysToExpire) {
+        var date = new Date();
+        date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+        document.cookie = cookieName + "=" + cookieValue + "; expires=" + date.toUTCString();
+    }
+
+    // Access a Cookie
+    function accessCookie(cookieName) {
+        var name = cookieName + "=";
+        var allCookieArray = document.cookie.split(';');
+        for (var i = 0; i < allCookieArray.length; i++) {
+            var temp = allCookieArray[i].trim();
+            if (temp.indexOf(name) == 0)
+                return temp.substring(name.length, temp.length);
+        }
+        return "";
+    }
+
+    function removeCookie(name) {
+        document.cookie = name;
+    }
+
+    function checkCookie(cookieName) {
+        var cookieCheck = accessCookie(cookieName);
+
+        if (cookieCheck = "") {
+            this.close();
+        } else if (cookieCheck != "") {
+            this.close();
+
+        } else {}
+    }
+
+
+    function exitIntent() {
+        document.addEventListener("mouseout", evt => {
+            if (evt.toElement === null && evt.relatedTarget === null) {
+                // An intent to exit has happened
+                console.log("Dont leave!")
+                this.open();
+
+            }
+        });
+    }
+
+
 
     function initializeEvents() {
 
@@ -132,6 +303,13 @@
             this.overlay.addEventListener('click', this.close.bind(this));
         }
 
+        // Exit Intent
+
+        if (this.exitIntent) {
+            this.exitIntent.addEventListener('mouseleave', this.open.bind(this));
+            console.log('Chumney')
+        }
+
     }
 
     function transitionSelect() {
@@ -139,6 +317,10 @@
         if (el.style.WebkitTransition) return "webkitTransitionEnd";
         if (el.style.OTransition) return "oTransitionEnd";
         return 'transitionend';
+    }
+
+    function cookie() {
+        var cookie = document.cookie;
     }
 
 }());
